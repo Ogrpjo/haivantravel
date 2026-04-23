45a62b3ce3f050b3cd702a40cd7a5f8232c76010
marzuzz <dopernguyen.work@gmail.com>
2026-04-23 15:40:26 +0700

    update frontend-user

--- haivantravel-frontend-user/app/components/layout/Navbar.tsx
+++ haivantravel-frontend-user/app/components/layout/Navbar.tsx
@@ -4,13 +4,7 @@ import { useState, useEffect, useRef } from "react";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/navbar";
 import { Link } from "@heroui/react";
 import { usePathname } from "next/navigation";
-import {
-  PhoneCall,
-  Facebook,
-  YouTube,
-  Menu,
-  X,
-} from "@deemlol/next-icons";
+import { PhoneCall, Facebook, YouTube, Menu, X } from "@deemlol/next-icons";
 import Image from "next/image";
 import ButtonGradient from "../button-gradient";
 
@@ -50,10 +44,18 @@ function TopNavbar({ socialMap }: { socialMap: SocialMap }) {
         <p className="pl-[20px]">Info.hcmc@haivantravelvn.com</p>
       </div>
       <div className="flex items-center md:px-[20px] gap-3 hidden sm:flex">
-        <Link href={socialMap.facebook} target="_blank" rel="noopener noreferrer">
+        <Link
+          href={socialMap.facebook}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Facebook size={20} />
         </Link>
-        <Link href={socialMap.youtube} target="_blank" rel="noopener noreferrer">
+        <Link
+          href={socialMap.youtube}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <YouTube size={20} />
         </Link>
         <Link href={socialMap.tiktok} target="_blank" rel="noopener noreferrer">
@@ -90,9 +92,9 @@ function BottomNavbar() {
   const [isServiceDropdownOpen, setIsServiceDropdownOpen] = useState(false);
   const [isMobileServiceOpen, setIsMobileServiceOpen] = useState(false);
   const menuItems = [
-    {name: "Trang chủ", href: "/"}, 
-    {name: "Case Study", href: "/case-study"},
-    {name: "Blog", href: "/blog"},
+    { name: "Trang chủ", href: "/" },
+    { name: "Case Study", href: "/case-study" },
+    { name: "Blog", href: "/blog" },
   ];
   const aboutItem = { name: "Về chúng tôi", href: "/about-us" };
   const serviceItems = [
@@ -120,7 +122,10 @@ function BottomNavbar() {
         <NavbarContent className="hidden lg:flex relative" justify="center">
           {menuItems.map((item, index) => (
             <NavbarItem key={`${item}-${index}`}>
-              <Link className="no-underline hover:text-white/70 text-[16px]" href={item.href}>
+              <Link
+                className="no-underline hover:text-white/70 text-[16px]"
+                href={item.href}
+              >
                 {item.name}
               </Link>
             </NavbarItem>
@@ -163,15 +168,21 @@ function BottomNavbar() {
             ) : null}
           </NavbarItem>
           <NavbarItem>
-            <Link className="no-underline hover:text-white/70 text-[16px]" href={aboutItem.href}>
+            <Link
+              className="no-underline hover:text-white/70 text-[16px]"
+              href={aboutItem.href}
+            >
               {aboutItem.name}
             </Link>
           </NavbarItem>
         </NavbarContent>
 
         <NavbarContent className="lg:px-[40px]" justify="end">
-          <Link href="/contact" className="no-underline decoration-transparent hover:no-underline">
-          <ButtonGradient name="Nhận Brief ngay" />
+          <Link
+            href="/contact"
+            className="no-underline decoration-transparent hover:no-underline"
+          >
+            <ButtonGradient name="Nhận Brief ngay" />
           </Link>
 
           <button
@@ -187,16 +198,18 @@ function BottomNavbar() {
       </Navbar>
 
       <div
-        className={`fixed inset-0 flex transition-opacity duration-300 ${isMenuOpen
+        className={`fixed inset-0 flex transition-opacity duration-300 ${
+          isMenuOpen
             ? "opacity-100 pointer-events-auto"
             : "opacity-0 pointer-events-none"
-          }`}
+        }`}
       >
         <div className="flex-1" onClick={() => setIsMenuOpen(false)} />
 
         <div
-          className={`w-full sm:max-w-sm bg-[#121212] text-white h-screen z-100 relative p-6 transform transition-transform duration-300 ${isMenuOpen ? "translate-x-0" : "translate-x-full"
-            }`}
+          className={`w-full sm:max-w-sm bg-[#121212] text-white h-screen z-100 relative p-6 transform transition-transform duration-300 ${
+            isMenuOpen ? "translate-x-0" : "translate-x-full"
+          }`}
         >
           <div className="flex justify-end mb-6">
             <button
@@ -212,7 +225,11 @@ function BottomNavbar() {
 
           <div className="flex flex-col gap-6">
             {menuItems.map((item, index) => (
-              <Link key={index} href={item.href} className="no-underline hover:text-white/70 cursor-pointer text-[16px]">
+              <Link
+                key={index}
+                href={item.href}
+                className="no-underline hover:text-white/70 cursor-pointer text-[16px]"
+              >
                 {item.name}
               </Link>
             ))}
@@ -254,11 +271,18 @@ function BottomNavbar() {
                 ))}
               </div>
             ) : null}
-            <Link href={aboutItem.href} className="no-underline hover:text-white/70 cursor-pointer text-[16px]">
+            <Link
+              href={aboutItem.href}
+              className="no-underline hover:text-white/70 cursor-pointer text-[16px]"
+            >
               {aboutItem.name}
             </Link>
-
-            <ButtonGradient name="Nhận Brief" />
+            <Link
+              href="/contact"
+              className="no-underline decoration-transparent hover:no-underline"
+            >
+              <ButtonGradient name="Nhận Brief ngay" />
+            </Link>
           </div>
         </div>
       </div>
@@ -339,8 +363,9 @@ export default function NavigationBar() {
     <>
       <div className="h-[148px] max-sm:h-[96px]" aria-hidden="true" />
       <section
-        className={`fixed top-0 left-0 w-full z-[100] bg-black transition-transform duration-300 ${isVisible ? "translate-y-0" : "-translate-y-full"
-          }`}
+        className={`fixed top-0 left-0 w-full z-[100] bg-black transition-transform duration-300 ${
+          isVisible ? "translate-y-0" : "-translate-y-full"
+        }`}
       >
         <TopNavbar socialMap={socialMap} />
         <BottomNavbar />

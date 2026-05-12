484306ff9e7d59f3c4d75a2bcc8504a9a33ed060
marzuzz <dopernguyen.work@gmail.com>
2026-05-12 18:13:53 +0700

    update frontend user

--- haivantravel-frontend-user/app/components/icons.tsx
+++ haivantravel-frontend-user/app/components/icons.tsx
@@ -1,103 +1,27 @@
+import Image from "next/image";
+
 export const EventIcon1 = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="35"
-    height="35"
-    viewBox="0 0 24 24"
-    fill="#ffffff"
-    className="icon icon-tabler icons-tabler-filled icon-tabler-calendar-event"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M16 2a1 1 0 0 1 .993 .883l.007 .117v1h1a3 3 0 0 1 2.995 2.824l.005 .176v12a3 3 0 0 1 -2.824 2.995l-.176 .005h-12a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-12a3 3 0 0 1 2.824 -2.995l.176 -.005h1v-1a1 1 0 0 1 1.993 -.117l.007 .117v1h6v-1a1 1 0 0 1 1 -1m3 8h-14v8.625c0 .705 .386 1.286 .883 1.366l.117 .009h12c.513 0 .936 -.53 .993 -1.215l.007 -.16zm-9 4a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1v-2a1 1 0 0 1 1 -1z" />
-  </svg>
+  <Image src="/icon/CoporateEvent.png" alt="Event icon" width={35} height={35}/>
 );
 
 export const EventIcon2 = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="35"
-    height="35"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="#ffffff"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    className="icon icon-tabler icons-tabler-outline icon-tabler-users"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M5 7a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
-    <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-    <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-    <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
-  </svg>
+  <Image src="/icon/Teambuilding.png" alt="Event icon" width={35} height={35}/>
 );
 
 export const EventIcon3 = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="35"
-    height="35"
-    viewBox="0 0 24 24"
-    fill="#ffffff"
-    className="icon icon-tabler icons-tabler-filled icon-tabler-mug"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M3.903 4.008l.183 -.008h10.828a2.08 2.08 0 0 1 2.086 2.077v.923h1.5c1.917 0 3.5 1.477 3.5 3.333v2.334c0 1.856 -1.583 3.333 -3.5 3.333h-1.663a5.33 5.33 0 0 1 -5.17 4h-4.334c-2.944 0 -5.333 -2.375 -5.333 -5.308v-8.618a2.08 2.08 0 0 1 1.903 -2.066m13.097 9.992h1.5c.843 0 1.5 -.613 1.5 -1.333v-2.334c0 -.72 -.657 -1.333 -1.5 -1.333h-1.5z" />
-  </svg>
+  <Image src="/icon/Conference.png" alt="Event icon" width={35} height={35}/>
 );
 
 export const EventIcon4 = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="35"
-    height="35"
-    viewBox="0 0 24 24"
-    fill="#ffffff"
-    className="icon icon-tabler icons-tabler-filled icon-tabler-star"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
-  </svg>
+  <Image src="/icon/Gala dinner.png" alt="Event icon" width={35} height={35}/>
 );
 
 export const EventIcon5 = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    fill="#ffffff"
-    height="30px"
-    width="30px"
-    version="1.1"
-    id="Layer_1"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 512 512"
-    enable-background="new 0 0 512 512"
-    stroke="#ffffff"
-  >
-    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-    <g
-      id="SVGRepo_tracerCarrier"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    ></g>
-    <g id="SVGRepo_iconCarrier">
-      {" "}
-      <path d="M244.4,69.8L174.5,0h-58.2l69.8,69.8H244.4z M395.6,69.8L325.8,0h-58.2l69.8,69.8H395.6z M418.9,0l69.8,69.8H512V0H418.9z M418.9,162.9h-93.1l69.8-69.8h-58.2l-69.8,69.8h-93.1l69.8-69.8h-58.2l-69.8,69.8H23.3l69.8-69.8H0v372.4 C0,491.1,20.9,512,46.5,512h418.9c25.7,0,46.5-20.9,46.5-46.5V93.1h-23.3L418.9,162.9z M23.3,0H0v69.8h93.1L23.3,0z"></path>{" "}
-    </g>
-  </svg>
+  <Image src="/icon/Media.png" alt="Event icon" width={35} height={35}/>
 );
 
 export const EventIcon6 = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="35"
-    height="35"
-    viewBox="0 0 24 24"
-    fill="#ffffff"
-    className="icon icon-tabler icons-tabler-filled icon-tabler-location"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M20.891 2.006l.106 -.006l.13 .008l.09 .016l.123 .035l.107 .046l.1 .057l.09 .067l.082 .075l.052 .059l.082 .116l.052 .096c.047 .1 .077 .206 .09 .316l.005 .106c0 .075 -.008 .149 -.024 .22l-.035 .123l-6.532 18.077a1.55 1.55 0 0 1 -1.409 .903a1.547 1.547 0 0 1 -1.329 -.747l-.065 -.127l-3.352 -6.702l-6.67 -3.336a1.55 1.55 0 0 1 -.898 -1.259l-.006 -.149c0 -.56 .301 -1.072 .841 -1.37l.14 -.07l18.017 -6.506l.106 -.03l.108 -.018z" />
-  </svg>
+  <Image src="/icon/MICE Travel.png" alt="Event icon" width={35} height={35}/>
 );
 
 export const MapMaker = (props: React.SVGProps<SVGSVGElement>) => (
